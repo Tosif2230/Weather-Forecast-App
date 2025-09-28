9b329e785ebdfcaf4ad856dd43f0e9ff7757f9ea
Tosif2230 <techie.tosif@gmail.com>
2025-09-28 11:17:52 +0530

    Implemented current weather fetch & rendering

--- script.js
+++ script.js
@@ -25,3 +25,34 @@ function formatLocalTime(unix, offset) {
   const mm = String(local.getUTCMinutes()).padStart(2,'0');
   return `${hh}:${mm}`;
 }
+// Implement for search
+async function fetchWeather(city) {}
+// Implement for geo-location.
+function fetchByLocation() {}
+//Render current weather
+function renderCurrentWeather(data) {
+  if (!data) return;
+  lastCurrent = data;
+  document.getElementById("tempNow").textContent = formatTemp(data.main.temp);
+  document.getElementById("descNow").textContent = data.weather[0].description;
+  document.querySelector(".weather-icon img").src = 
+    `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+  humidityVal.textContent = `${data.main.humidity}%`;
+  pressureVal.textContent = `${data.main.pressure} hPa`;
+  visibilityVal.textContent = `${(data.visibility / 1000).toFixed(1)} km`;
+  windspeedVal.textContent = `${data.wind.speed} m/s`;
+  feelsVal.textContent = `${tempUnit==='C'?kToC(data.main.feels_like):kToF(data.main.feels_like)}°${tempUnit}`;
+
+  document.getElementById("dateNow").textContent = new Date(data.dt*1000).toLocaleDateString();
+  document.getElementById("locNow").textContent = `${data.name}, ${data.sys.country}`;
+  document.getElementById("sunriseVal").textContent = formatLocalTime(data.sys.sunrise, data.timezone);
+  document.getElementById("sunsetVal").textContent = formatLocalTime(data.sys.sunset, data.timezone);
+
+  //heat alert if >40°C
+  applyWeatherBackground(data.weather[0].main);
+  if (kToC(data.main.temp) > 40) showError("⚠️ Heat Alert: Stay Hydrated!");
+
+  fetchAirQuality(data.coord.lat, data.coord.lon);
+}
+function applyWeatherBackground(main) {}
